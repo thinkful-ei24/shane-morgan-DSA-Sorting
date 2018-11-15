93dc63a525043dbc88b29f057b172a2df2bbd5ad
Morgan Newman <mpnewman95@gmail.com>
2018-11-15 11:09:07 -0800

    Adds quicksort

--- sorting-drills.js
+++ sorting-drills.js
@@ -1,12 +1,140 @@
+const data = [
+  89,
+  30,
+  25,
+  32,
+  72,
+  70,
+  51,
+  42,
+  25,
+  24,
+  53,
+  55,
+  78,
+  50,
+  13,
+  40,
+  48,
+  32,
+  26,
+  2,
+  14,
+  33,
+  45,
+  72,
+  56,
+  44,
+  21,
+  88,
+  27,
+  68,
+  15,
+  62,
+  93,
+  98,
+  73,
+  28,
+  16,
+  46,
+  87,
+  28,
+  65,
+  38,
+  67,
+  16,
+  85,
+  63,
+  23,
+  69,
+  64,
+  91,
+  9,
+  70,
+  81,
+  27,
+  97,
+  82,
+  6,
+  88,
+  3,
+  7,
+  46,
+  13,
+  11,
+  64,
+  76,
+  31,
+  26,
+  38,
+  28,
+  13,
+  17,
+  69,
+  90,
+  1,
+  6,
+  7,
+  64,
+  43,
+  9,
+  73,
+  80,
+  98,
+  46,
+  27,
+  22,
+  87,
+  49,
+  83,
+  6,
+  39,
+  42,
+  51,
+  54,
+  84,
+  34,
+  53,
+  78,
+  40,
+  14,
+  5
+];
 
-const data = [89,30,25,32,72,70,51,42,25,24,53,55,78,50,13,40,48,32,26,2,14,33,45,72,56,44,21,88,27,68,15,62,93,98,73,28,16,46,87,28,65,38,67,16,85,63,23,69,64,91,9,70,81,27,97,82,6,88,3,7,46,13,11,64,76,31,26,38,28,13,17,69,90,1,6,7,64,43,9,73,80,98,46,27,22,87,49,83,6,39,42,51,54,84,34,53,78,40,14,5];
+function qSort(array, start = 0, end = data.length) {
+  if (start >= end) {
+    return array;
+  }
 
-function qSort(data) {
+  const middle = partition(array, start, end);
+  array = qSort(array, start, middle); // 0, 3
+  array = qSort(array, middle + 1, end);
+  return array;
+}
 
+function partition(array, start, end) {
+  const pivot = array[end - 1];
+  let j = start;
+
+  for (let i = start; i < end - 1; i++) {
+    if (array[i] <= pivot) {
+      swap(array, i, j);
+      j++;
+    }
+  }
+
+  swap(array, end - 1, j);
+  return j;
+}
+
+function swap(arr, a, b) {
+  const temp = arr[a];
+  arr[a] = arr[b];
+  arr[b] = temp;
 }
 
 function mSort(data) {
-  if(data.length <= 1) {
+  if (data.length <= 1) {
     return data;
   }
 
@@ -22,8 +150,8 @@ function merge(left, right, data) {
   let rightIndex = 0;
   let outputIndex = 0;
 
-  while(leftIndex < left.length && rightIndex < right.length) {
-    if(left[leftIndex] < right[rightIndex]) {
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
       data[outputIndex++] = left[leftIndex++];
     } else {
       data[outputIndex++] = right[rightIndex++];
@@ -38,4 +166,4 @@ function merge(left, right, data) {
   return data;
 }
 
-console.log(mSort(data));
+console.log(qSort(data));
